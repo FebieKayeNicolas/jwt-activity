06031534c49a142622ea5897639a90216f9cc645
FebieNicolas <32101304+FebieKayeNicolas@users.noreply.github.com>
2019-03-15 17:51:47 +0800

    Add files via upload

---
+++ jwt-activity/php/validate.php
@@ -0,0 +1,28 @@
+<?php
+            require_once('connect.php');
+            require_once('generate.php');
+
+            session_start();
+            $jwt = $_SESSION['jwt'];    
+            $id = $_SESSION['id'];
+
+            validateToken($id, $jwt, $conn);
+
+            function validateToken($id, $userToken, $conn){
+                
+                $existingToken = generateToken($id, 1, $conn);
+
+                echo "Existing token: ". $existingToken;
+
+        
+                if($userToken===$existingToken){
+                    echo "<br> TOKEN IS VALID";
+                    echo "<br>";
+                    echo "$userToken";
+                }else{
+                    echo "<br> TOKEN IS INVALID";
+                    echo "<br>";
+                    echo "$userToken";
+                }
+            }
+?>
